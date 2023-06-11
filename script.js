c561926ec8ac6ef94ac3ac5d9700ae3ac3f5e5cc
Bruno Santos <wanderley.bruno@gmail.com>
2023-06-10 22:15:56 -0300

    projeto final parte 01

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+var nome = prompt("Como gostaria e ser chamado ?")
+alert("Bem vindo, " + nome + " !")
+*/
+function gerarTabuada() {
+    var numero = parseInt(document.getElementById("numero").value);
+    var tabuadaHTML = "";
+
+    for (var i = 1; i <= 10; i++) {
+      var resultado = numero * i;
+      var linhaTabuada = document.createElement("div");
+      linhaTabuada.textContent = numero + " x " + i + " = " + resultado;
+      linhaTabuada.classList.add("tabuada-item");
+      linhaTabuada.style.backgroundColor = "green";
+      linhaTabuada.style.marginTop = "5px";
+      linhaTabuada.style.textAlign = "center"
+      linhaTabuada.style.width = "auto"
+      
+      document.getElementById("tabuada").appendChild(linhaTabuada);
+      var tabuadaToda = document.createElement("div")
+      
+    }
+  }
